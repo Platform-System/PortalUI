7bae374bcf70736dd18e65eb8c241a1da15b7ae5
Khánh Hưng <khanhhungzm@gmail.com>
2026-05-21 20:28:18 +0700

    build: add react-is and add next transpilePackages for design-system

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  transpilePackages: ["@platform/design-system"],
   experimental: {
     externalDir: true,
   },
